7d53df86d9f160feafdf68bd3c493dae6019d8b8
ryan/bradley.cowan <ryan.bradley.cowan@gmail.com>
2021-07-19 23:43:26 +0100

    Blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    # these field types come from https://docs.djangoproject.com/en/3.2/ref/models/fields/
+    # title, pub_date, body, image
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
